a6b3d072a3857ea1f919e6048e5230dfe053158e
gtechera <36115335+gtechera@users.noreply.github.com>
2018-02-21 08:12:48 -0300

    Update server.js
    Change db Password

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
     connection: {
         host: '127.0.0.1',
         user: 'postgres',
-        password: 'Condor88',
+        password: 'DBOSCO',
         database: 'smart-brain'
     }
 });
